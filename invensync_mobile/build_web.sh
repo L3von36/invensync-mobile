7ff43777908a4354cddf35ee1336638c977ba892
Z User <z@container>
2026-07-01 07:58:37 +0000

    c976688d-2aa3-4842-ac20-393999bb1436

--- invensync_mobile/build_web.sh
+++ invensync_mobile/build_web.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd /home/z/my-project/invensync_mobile
-/home/z/flutter/bin/flutter build web --release 2>&1
+/home/z/flutter/bin/flutter build web --release --base-href "/invensync-mobile/" 2>&1
+echo "BUILD EXIT: $?"
